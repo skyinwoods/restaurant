282b21b493097f42d88f4af14f909a41be2911c7
skyinwoods <skyinwoods@gmail.com>
2022-04-10 14:53:17 +0800

    bootstrap ver@4.6.1

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 // routes setting
-app.get('/restaurants', (req, res) => {
+app.get('/', (req, res) => {
   res.render('index', {restaurant: resList.results} )
 })
 
